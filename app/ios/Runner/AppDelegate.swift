ff7a15240a56324e0d4e312a612da8e67f3d86df
Grobi112 <mikefuellbeck@gmail.com>
2026-03-10 19:38:27 +0100

    Fix: AppDelegate - UNUserNotificationCenter.delegate + Tippfehler GeneratedPluginRegistrant

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,18 +1,15 @@
 import Flutter
 import UIKit
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Explizit für Remote Notifications registrieren – nötig für FCM/APNs auf iOS
-    application.registerForRemoteNotifications()
+    GeneratedPluginRegistrant.register(with: self)
+    UNUserNotificationCenter.current().delegate = self
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
